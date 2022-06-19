c623ced2776d76296d4e85d6195c6b415cff71b2
andrei <andreisushkov@mail.ru>
2022-06-19 21:08:23 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,40 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] item = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item name = items[i];    
+            if (items[i] != null) {
+                    item[size] = name;
+                    size++;
+                }
+        }
+        item = Arrays.copyOf(item, size);
+        for (int i = 0; i < item.length; i++) {
+            System.out.println(item[i]);
+        }
+        return item;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] item1 = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item name1 = items[i];
+            if (name1 != null && name1.getName().equals(key)) {
+                item1[size] = name1;
+                size++;
+            }
+        }
+        item1 = Arrays.copyOf(item1, size);
+        for (int i = 0; i < item1.length; i++) {
+            System.out.println(item1[i]);
+        }
+        return item1;
     }
 
     public Item findById(int id) {
