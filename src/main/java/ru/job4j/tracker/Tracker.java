5e8efb5c60650138f8a0ef14ea80d1efe46d04dc
andrei <andreisushkov@mail.ru>
2022-06-30 18:36:14 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #316584]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,8 +60,8 @@ public class Tracker {
         int index = indexOf(id);
         if (index != -1) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
-        items[size - 1] = null;
-        size--;
+            items[size - 1] = null;
+            size--;
         }
         return index != -1;
     }
